43a6073fcc9111e345b3e009889990da7d391afc
Ben Donaghy <bendonaghy321@gmail.com>
2023-09-29 23:16:34 +0100

    begin working on heap allocated strings

---
+++ interpreter.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // when declaring a pointer variable, we need to specify the type of data it will point to (char *str or char* str or char * str)
+    char *str = (char *)malloc(20 * sizeof(char));
+}

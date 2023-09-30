8b16fc53ac17ae98ab35625d23d059b77d3c8484
Ben Donaghy <bendonaghy321@gmail.com>
2023-09-30 22:23:24 +0100

    add node struct

--- interpreter.c
+++ interpreter.c
@@ -1,9 +1,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
     // when declaring a pointer variable, we need to specify the type of data it will point to (char *str or char* str or char * str)
     char *str = (char *)malloc(20 * sizeof(char));
+
+    if (str == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(str, "Hello, world!\n");
+
+    free(str);
 }
---
+++ node.c
@@ -0,0 +1,4 @@
+typedef struct Node {
+    int data;
+    struct node *next;
+} Node; // this is part of the typedef syntax for aliasing a struct

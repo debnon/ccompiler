2d21b09aace6440896c99f211ea6a077a67bca72
Ben Donaghy <bendonaghy321@gmail.com>
2023-10-01 16:46:02 +0100

    memory alloc. and string copy fixes

--- nodeOperations.c
+++ nodeOperations.c
@@ -13,13 +13,14 @@ Node* createNode(char *newStr) {
         exit(EXIT_FAILURE);
     }
 
-    node->str = (char *)malloc(sizeof(newStr));// like node.str but where we have a pointer to node. 
-    if (node == NULL) {
+    node->str = (char *)malloc((strlen(newStr) + 1) * sizeof(char));// like node.str but where we have a pointer to node. 
+    if (node->str == NULL) {
         fprintf(stderr, "String node init. memory alloc. failed\n");
         free(node);
         exit(EXIT_FAILURE);
     }
-
+    strcpy(node->str, newStr);
+    
     node->prev = NULL;
     node->next = NULL;
 
